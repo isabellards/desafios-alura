6d0b61896bc894c471250de0d79a89616bfb561d
isabellards <isabellards.c@gmail.com>
2024-02-26 16:12:33 -0300

    :sparkles:

--- day-two/script.js
+++ day-two/script.js
@@ -3,12 +3,11 @@ function setInvisible(studyQuestion) {
 }
 
 function showMessage() {
-  console.log('chegou na funcao');
   const name = document.getElementById('name').value;
   const age = document.getElementById('age').value;
   const language = document.getElementById('language').value;
 
-  const message =
+  const messageOne =
     'Olá, ' +
     name +
     ', você tem ' +
@@ -16,5 +15,25 @@ function showMessage() {
     ' e já está aprendendo ' +
     language +
     '!';
-  console.log(message);
+  console.log(messageOne);
+
+  setInvisible('studyQuestion');
+
+  document.getElementById('studyQuestion').style.display = 'block';
+}
+
+function sendAnswer() {
+  const answerStudyQuestion = document.getElementById('answerStudyQuestion');
+
+  if (answerStudyQuestion == 1) {
+    const successMessage =
+      'Muito bom! Continue estudando e você terá muito sucesso.';
+    console.log(successMessage);
+    document.getElementById('finalMessage').value = successMessage;
+  } else {
+    const failureMessage =
+      'Ahh que pena... Já tentou aprender outras linguagens?';
+    console.log(failureMessage);
+    document.getElementById('finalMessage').value = failureMessage;
+  }
 }
