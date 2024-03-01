1f7ebd4d923d3b1d1844b9f52454c562e0b00efc
isabellards <isabellards.c@gmail.com>
2024-02-29 21:13:08 -0300

    :sparkles:

--- day-three/script.js
+++ day-three/script.js
@@ -1,1 +1,13 @@
-function initialQuestion() {}
+function setInvisible(frontTwo) {
+  document.getElementById('frontTwo').style.display = 'none';
+}
+
+function setInvisible(backTwo) {
+  document.getElementById('backTwo').style.display = 'none';
+}
+
+function initialQuestion() {
+  if (initialFront === true) {
+  } else {
+  }
+}
