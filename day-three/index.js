98fa705b83cbb6b79c9ecb94dc2b19726620828d
isabellards <isabellards.c@gmail.com>
2024-03-02 17:37:34 -0300

    :sparkles:

--- day-three/index.js
+++ day-three/index.js
@@ -1,27 +1,25 @@
-const area = prompt(
-  "Você quer seguir para área de 'Front-End' ou de 'Back-End'? Digite o nome da área: "
-);
-
+const area = prompt('Você deseja seguir para área de Front-End ou Back-End?');
 let linguagem = '';
 
 if (area === 'Front-End') {
-  linguagem = prompt('Você quer aprender React ou Vue?');
+  linguagem = prompt('E você deseja aprender React ou Vue?');
 } else if (area === 'Back-End') {
-  linguagem = prompt('Você quer aprender C# ou Java?');
+  linguagem = prompt('E você deseja aprender C# ou Java?');
 } else {
-  alert('Você não inseriu uma área válida!');
+  alert('Área Inválida');
 }
 
-const especialidadeOuFullstack = prompt(
-  'Digite 1 para seguir se especializando na área escolhida ou 2 para seguir se desenvolvendo para se tornar Fullstack'
+const especialidadeFullstack = prompt(
+  'Digite 1 se gostaria de seguir se especializando na área ou 2 para tornar-se Fullstack'
 );
-if (especialidadeOuFullstack == 1) {
+
+if (especialidadeFullstack == '1') {
   alert(
-    `Continue se especializando em ${linguagem} para dominar a área de ${area}!`
+    `Continue se especializando em ${linguagem} para dominar a área ${area}!`
   );
-} else if (especialidadeOuFullstack == 2) {
+} else if (especialidadeFullstack == '2') {
   alert(
-    `Chegou a hora de começar a aprender outras linguagens além de ${linguagem} se você quer se tornar Fullstack!`
+    `Chegou a hora de começar a aprender outras linguagens além de ${linguagem} se quer se tornar Fullstack!`
   );
 } else {
   alert('Você não inseriu um valor válido!');
@@ -30,10 +28,4 @@ if (especialidadeOuFullstack == 1) {
 let msg = prompt(
   "Tem mais alguma tecnologia que você gostaria de aprender? Digite 'ok' em caso positivo."
 );
-while (msg === 'ok') {
-  let novaTecnologia = prompt('Qual?');
-  alert(`${novaTecnologia} é realmente uma tecnologia muito legal!`);
-  msg = prompt(
-    "Tem mais alguma tecnologia que você gostaria de aprender? Digite 'ok' em caso positivo."
-  );
-}
+while (msg == 'ok') {}
